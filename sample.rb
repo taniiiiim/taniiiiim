3678c2279b96d6e6288cde314ff49662615149b4
vagrant <vagrant@localhost.localdomain>
2019-08-09 04:30:24 +0000

    first commit

---
+++ sample.rb
@@ -0,0 +1,30 @@
+#!/usr/local/bin/ruby
+
+print "Content-Type: text/html\n"
+print "\n"
+
+require "cgi"
+require "cgi/session"
+require 'cgi/session/pstore'
+
+cgi = CGI.new
+
+begin
+  session = CGI::Session.new(cgi,{"new_session"=>false})
+rescue ArgumentError
+  session = nil
+end
+
+if session != nil
+  session['count'] = session['count'].to_i + 1
+  session.close
+else
+  session = CGI::Session.new(cgi,{"new_session"=>true})
+  session['count'] ="1"
+  session.close
+end
+
+header = cgi.header({"charset"=>"UTF-8"})
+puts header
+puts "Count is #{session['count']}"
+
